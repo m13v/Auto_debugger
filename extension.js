2fe94b905fdaf2f7f7d8c33284669ce16bf3fc0b
matthew-heartful <matthew.heartful@gmail.com>
2024-05-11 15:09:00 -0700

    chat interface

--- extension.js
+++ extension.js
@@ -1,36 +1,77 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+    let disposable = vscode.commands.registerCommand('simple-extension.helloWorld', function () {
+        vscode.window.showInformationMessage('Hello World from Simple Extension!');
+    });
+    
+    context.subscriptions.push(
+        vscode.commands.registerCommand('yourExtension.showChat', () => {
+            const panel = vscode.window.createWebviewPanel(
+                'chatSidebar', // Identifies the type of the webview. Used internally
+                'Chat', // Title of the panel displayed to the user
+                vscode.ViewColumn.Three, // Editor column to show the new webview panel in, set to Three for right side.
+                {
+                    enableScripts: true // Enable scripts in the webview
+                }
+            );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "auto-debugger" is now active!');
+            panel.webview.html = getWebviewContent();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-debugger.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from auto_debugger!');
-	});
-
-	context.subscriptions.push(disposable);
+            // Register message handler within the scope of the created panel
+            panel.webview.onDidReceiveMessage(
+                message => {
+                    switch (message.command) {
+                        case 'sendMessage':
+                            handleSendMessage(message.text);
+                            return;
+                    }
+                },
+                undefined,
+                context.subscriptions
+            );
+        })
+    );
 }
 
-// This method is called when your extension is deactivated
+function handleSendMessage(text) {
+    // Handle sending the message, e.g., display it in the chat or send to a server
+    console.log('Message from webview:', text);
+}
+
+function getWebviewContent() {
+    // HTML content for the webview
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>Chat Interface</h1>
+        <div id="chat"></div>
+        <input type="text" id="messageInput">
+        <button onclick="sendMessage()">Send</button>
+
+        <script>
+            const vscode = acquireVsCodeApi();
+            function sendMessage() {
+                const input = document.getElementById('messageInput');
+                vscode.postMessage({
+                    command: 'sendMessage',
+                    text: input.value
+                });
+                input.value = '';
+            }
+        </script>
+    </body>
+    </html>`;
+}
+
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
