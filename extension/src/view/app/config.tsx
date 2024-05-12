79e0aa7d9ad34c10a78e29a9a6c6b9b2a2137971
matthew-heartful <matthew.heartful@gmail.com>
2024-05-12 01:14:13 -0700

    array of messages

--- extension/src/view/app/config.tsx
+++ extension/src/view/app/config.tsx
@@ -1,18 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
-
-import { type IConfig,
-  // type IUser, type ICommand, CommandAction
- } from "./model";
+import { type IConfig } from "./model";
 import Chat, { Message } from './Chat';
 
 interface IConfigProps {
   vscode: any;
   initialData: IConfig;
 }
-
-// interface IConfigState {
-//   config: IConfig;
-// }
 
 const Config = ({ vscode, initialData }: IConfigProps) => {
   // const [config, setConfig] = useState<IConfig>(() => {
@@ -26,17 +19,20 @@ const Config = ({ vscode, initialData }: IConfigProps) => {
     const handleMessage = (event: MessageEvent) => {
       console.log('Webview received message:', event.data);
 
-      if (event.data.command === 'message') {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { type: 'assistant', text: event.data.text, code: '' }
-        ]);
-      }
+      // Assuming the message from the event should also be displayed in the chat
+      const incomingMessage: Message = {
+        type: 'assistant', // Assuming all incoming messages are from the assistant
+        text: event.data.text,
+        code: event.data.code // Assuming there's a 'code' field in the incoming data
+      };
 
-      // vscode.postMessage({
-      //   command: 'pong',
-      //   text: 'Pong from webview'
-      // });
+      // Update messages state to include the incoming message
+      setMessages(prevMessages => [...prevMessages, incomingMessage]);
+
+      vscode.postMessage({
+        command: 'pong',
+        text: 'Pong from webview'
+      });
     };
 
     window.addEventListener('message', handleMessage);
@@ -47,32 +43,25 @@ const Config = ({ vscode, initialData }: IConfigProps) => {
     };
   }, [vscode]);
 
-  const onSendMessage = useCallback((message: string) => {
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { type: 'user', text: message }
-    ]);
+  // useEffect(() => {
+  //   console.log('Updated messages:', messages);
+  // }, [messages]); // This useEffect runs every time `messages` changes
 
+  const onSendMessage = useCallback((message: string) => {
+    const newMessage: Message = {
+      type: 'user',
+      text: message
+    };
+
+    // Update local messages state
+    setMessages(prevMessages => [...prevMessages, newMessage]);
+
+    // Send message to vscode
     vscode.postMessage({
       command: 'message',
       text: message
     });
   }, [vscode]);
-
-  /*
-  const defineState = (newState: IConfigState) => {
-    setConfig(newState);
-    vscode.setState(newState);
-  };
-
-  const saveConfig = () => {
-    const command: ICommand = {
-      action: CommandAction.Save,
-      content: config
-    };
-    vscode.postMessage(command);
-  };
-  */
 
   return (
     <React.Fragment>
@@ -82,4 +71,3 @@ const Config = ({ vscode, initialData }: IConfigProps) => {
 };
 
 export default Config;
-
