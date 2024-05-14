8576912488855568c2fafcb8a50421507d6ff951
matthew-heartful <matthew.heartful@gmail.com>
2024-05-13 17:42:57 -0700

    Revert the revert.
    This reverts commit aa90518db08a7bb493502aab6b5d7497599fb123.

--- vscode-react/out/view/app/model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommandAction = void 0;
-var CommandAction;
-(function (CommandAction) {
-    CommandAction[CommandAction["Save"] = 0] = "Save";
-})(CommandAction || (exports.CommandAction = CommandAction = {}));
-//# sourceMappingURL=model.js.map
